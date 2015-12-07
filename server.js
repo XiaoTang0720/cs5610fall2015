049cd026cb8b30afe4bf30320cec338e4bae99b1
XiaoTang <tang.x@husky.neu.edu>
2015-12-06 23:00:55 -0800

    server configuration fix.

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ var multer = require('multer');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cs5610');
-var db = mongoose.connection;
+
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
 
 app.use(express.static(__dirname + '/public'));
 
