c07f635be3ab4bea2f46abad9b104502fde89027
XiaoTang <tang.x@husky.neu.edu>
2015-12-06 22:45:05 -0800

    Bug fix.

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cs5610');
 var db = mongoose.connection;
 
-console.log(mongoose);
-
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
@@ -18,9 +16,6 @@ app.use(multer());
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-//require("./public/assignment/server/services/form.service.js")(app);
-//require("./public/assignment/server/services/user.service.js")(app);
-//require("./public/assignment/server/services/field.service.js")(app);
 require("./public/assignment/server/app.js")(app, mongoose, db);
 require("./public/project/server/app.js")(app, mongoose, db);
 
