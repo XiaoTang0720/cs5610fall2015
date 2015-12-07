9cb90fbd18f12d68429b78150bb482c922b3cf82
XiaoTang <tang.x@husky.neu.edu>
2015-12-06 23:59:56 -0800

    test

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 var express = require('express');
+var multer = require('multer');
 var app = express();
+app.use(multer());
 app.use(express.static(__dirname + '/public'));
-
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var multer = require('multer');
-
 var connectionString = 'mongodb://localhost/cs5610';
-
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
@@ -18,18 +15,12 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 }
-
 mongoose.connect(connectionString);
 var db = mongoose.connection;
-
-
+app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(multer());
 
 require("./public/assignment/server/app.js")(app, mongoose, db);
 require("./public/project/server/app.js")(app, mongoose, db);
 
-app.listen(port , ipaddress);
-
-console.log('Server now is listen to 127.0.0.1:3000');
+app.listen(port, ipaddress);
